a05bf2847cd3fe71e11a7b3fbc101cd47cace038
SakaiTaka23 <tt3.sakai@gmail.com>
2021-07-22 14:00:34 +0900

    route定義

--- backend/infrastructure/server/router.go
+++ backend/infrastructure/server/router.go
@@ -3,37 +3,38 @@ package server
 import (
 	"backend/infrastructure/middleware"
 	"backend/injector"
-	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetRouter(app *fiber.App) *fiber.App {
 	middleware.SetAppMiddleware(app)
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello World!",
-		})
-	})
-
-	app.Get("/public", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Public",
-		})
-	})
-
 	userHandler := injector.InjectUserHandler()
 
-	private := app.Group("/", middleware.AuthMiddleware)
-	private.Get("/private", func(c *fiber.Ctx) error {
-		log.Println(c.Locals("user"))
+	app.Get("/user/:username", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message": "Private",
+			"message": "get user info by user id",
+		})
+	})
+	user := app.Group("/user", middleware.AuthMiddleware)
+	user.Post("/", userHandler.CreateUser)
+	user.Patch("/:username", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "update user infomation",
 		})
 	})
 
-	private.Post("/create-user", userHandler.CreateUser)
+	schedule := app.Group("/schedule", middleware.AuthMiddleware)
+	schedule.Post("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "post schedule create",
+		})
+	})
+	schedule.Patch("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "delete schedule",
+		})
+	})
 
 	return app
 }
