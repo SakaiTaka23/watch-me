c219bb00ecf3ed773fdfed9e54791c4e7a1c777c
SakaiTaka23 <tt3.sakai@gmail.com>
2021-07-25 12:53:10 +0900

    型を最適化

--- backend/entity/model/schedule.go
+++ backend/entity/model/schedule.go
@@ -4,9 +4,9 @@ type Schedule struct {
 	ID        string `json:"id"`
 	About     string `json:"about"`
 	Emoji     string `json:"emoji" gorm:"not null"`
-	Year      uint   `json:"year" gorm:"not null"`
-	Month     uint   `json:"month" gorm:"not null"`
-	Day       uint   `json:"day" gorm:"not null"`
+	Year      uint16 `json:"year" gorm:"not null"`
+	Month     uint8  `json:"month" gorm:"not null"`
+	Day       uint8  `json:"day" gorm:"not null"`
 	StartTime string `json:"start_time" gorm:"not null"`
 	EndTime   string `json:"end_time"`
 	Place     string `json:"place"`
