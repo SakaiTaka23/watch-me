621a8cdd34588e88ba22f0ff85dad8b55dca266e
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-01 13:45:18 +0900

    スケジュール・基本的なふぇっちゃー定義

---
+++ frontend/types/model/schedule.ts
@@ -0,0 +1,15 @@
+type Schedule = {
+  id: string;
+  about: string;
+  emoji: string;
+  year: number;
+  month: number;
+  day: number;
+  start_time: string;
+  end_time: string;
+  place: string;
+  title: string;
+  url: string;
+};
+
+export type { Schedule };
