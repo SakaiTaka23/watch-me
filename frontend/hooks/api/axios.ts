f494e082b78723fa6c042b2bbab79724f7ba4785
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-03 18:15:03 +0900

    axiosインスタンスを改善

--- frontend/hooks/api/axios.ts
+++ frontend/hooks/api/axios.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosInstance } from 'axios';
 
-let axiosInstance: AxiosInstance = axios.create({
+const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'http://127.0.0.1:5000',
-  timeout: 1000,
   headers: { 'Content-Type': 'application/json' },
 });
 
-export default axiosInstance;
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+export { axiosInstance, fetcher };
