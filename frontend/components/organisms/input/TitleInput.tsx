9fbbb7999839695f2d1c07ca51027d7fe80c0b10
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-26 18:12:30 +0900

    titleのレジスター名変更

--- frontend/components/organisms/input/TitleInput.tsx
+++ frontend/components/organisms/input/TitleInput.tsx
@@ -11,7 +11,7 @@ const TitleInput = () => {
       margin='normal'
       placeholder='Title *'
       fullWidth
-      {...register('title,', {
+      {...register('title', {
         required: true,
       })}
     />
