077e6bbf0d1ac4f3f43d79d2f3c845207a3f3d9a
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-26 22:02:00 +0900

    エラー表示を行うようにし文字数も常に表示するようにした

--- frontend/components/organisms/input/AboutInput.tsx
+++ frontend/components/organisms/input/AboutInput.tsx
@@ -1,21 +1,39 @@
-import { TextField } from '@material-ui/core';
-import React from 'react';
+import { TextField, Typography } from '@material-ui/core';
+import React, { ChangeEvent } from 'react';
+import { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 const AboutInput = () => {
-  const { register } = useFormContext();
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const [count, setCount] = useState(0);
+
+  const countWord = (e: ChangeEvent<HTMLInputElement>) => {
+    setCount(e.target.value.length);
+  };
 
   return (
-    <TextField
-      variant='outlined'
-      margin='normal'
-      placeholder='About'
-      minRows='5'
-      maxRows='10'
-      multiline
-      fullWidth
-      {...register('about')}
-    />
+    <>
+      <TextField
+        variant='outlined'
+        margin='normal'
+        placeholder='About'
+        minRows='5'
+        maxRows='5'
+        helperText={`${count} / 250`}
+        multiline
+        fullWidth
+        {...register('about', { maxLength: 250 })}
+        onChange={countWord}
+      />
+      {errors.about && (
+        <Typography color='error' variant='overline'>
+          about must be 250 words or shorter
+        </Typography>
+      )}
+    </>
   );
 };
 
