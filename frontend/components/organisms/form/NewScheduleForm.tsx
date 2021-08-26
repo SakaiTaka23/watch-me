85bede4d301747fd96eba2120871b067f9a47ecd
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-26 18:14:04 +0900

    現在開発中のページに反映させた

---
+++ frontend/components/organisms/form/NewScheduleForm.tsx
@@ -0,0 +1,61 @@
+import { Container, makeStyles } from '@material-ui/core';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import SubmitButton from '../../molecules/SubmitButton';
+import AboutInput from '../input/AboutInput';
+import EmojiInput from '../input/EmojiInput';
+import PeriodInput from '../input/PeriodInput';
+import PlaceInput from '../input/PlaceInput';
+import TitleInput from '../input/TitleInput';
+import URLInput from '../input/URLInput';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const handleClick = (data: any) => {
+  console.log('clicked');
+  console.log(data);
+  // createSchedule(testAllSchedule);
+  // console.log(error);
+};
+
+const NewScheduleForm = () => {
+  const classes = useStyles();
+  const methods = useForm();
+  return (
+    <Container maxWidth='md'>
+      <div className={classes.paper}>
+        <FormProvider {...methods}>
+          <form className={classes.form} onSubmit={methods.handleSubmit(handleClick)}>
+            <TitleInput />
+            {/* <EmojiInput /> */}
+            {/* <PeriodInput /> */}
+            <AboutInput />
+            <PlaceInput />
+            <URLInput />
+            <SubmitButton />
+          </form>
+        </FormProvider>
+      </div>
+    </Container>
+  );
+};
+
+export default NewScheduleForm;
--- frontend/pages/schedule/new.tsx
+++ frontend/pages/schedule/new.tsx
@@ -1,108 +1,17 @@
-import { Container, Typography, TextField, Button, MenuItem, Select } from '@material-ui/core';
-import { Picker } from 'emoji-mart';
+import { Container, Typography } from '@material-ui/core';
 import React from 'react';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import useCreateSchedule from '../../hooks/api/schedule/useCreateSchedule';
-import { Schedule } from '../../types/model/schedule';
-import 'emoji-mart/css/emoji-mart.css';
-import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
+import NewScheduleForm from '../../components/organisms/form/NewScheduleForm';
 
 const New = () => {
-  const { createSchedule, id, error } = useCreateSchedule();
-  const { handleSubmit, register } = useForm();
-  const [emoji, setEmoji] = useState<string>('');
-  const [year, setYear] = useState(2021);
-  const [selectedStartingDate, handleStartingDateChange] = useState(new Date());
-  const [selectedEndingDate, handleEndingDateChange] = useState(new Date());
-
-  const handleClick = (data: any) => {
-    console.log('clicked');
-    console.log(data);
-    // createSchedule(testAllSchedule);
-    // console.log(error);
-  };
-
-  const handleYearChange = (e) => {
-    setYear(e.target.value as number);
-    console.log(year);
-  };
-
   return (
-    <>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <Container maxWidth='md'>
-          <div>
-            <Typography component='h1' variant='h5'>
-              Create New Schedule
-            </Typography>
-            <form onSubmit={handleSubmit(handleClick)}>
-              <TextField
-                variant='outlined'
-                margin='normal'
-                placeholder='Title *'
-                fullWidth
-                {...register('title', {
-                  required: true,
-                })}
-              />
-              <div suppressHydrationWarning={true}>
-                {process.browser && (
-                  <Picker
-                    onSelect={(e) => {
-                      setEmoji(e.id);
-                    }}
-                  />
-                )}
-              </div>
-              <TextField
-                value={emoji}
-                variant='outlined'
-                margin='normal'
-                placeholder='Please Pick An Emoji'
-                fullWidth
-                {...register('emoji', {
-                  required: true,
-                })}
-              />
-              <TextField
-                variant='outlined'
-                margin='normal'
-                placeholder='About'
-                minRows='5'
-                maxRows='10'
-                multiline
-                fullWidth
-                {...register('about')}
-              />
-              <TextField variant='outlined' margin='normal' placeholder='Place' fullWidth {...register('place')} />
-              <TextField variant='outlined' margin='normal' placeholder='URL' fullWidth {...register('url')} />
-              {/* 2021~2025の間の範囲に収まるべき */}
-              <input type='number' value={year} readOnly {...register('year')} />
-              {/* <DatePicker
-                disableToolbar
-                variant='inline'
-                label='Only calendar'
-                helperText='No year selection'
-                value={selectedStartingDate}
-                onChange={handleStartingDateChange}
-              />
-
-              <DatePicker
-                disableToolbar
-                variant='inline'
-                label='Only calendar'
-                helperText='No year selection'
-                value={selectedEndingDate}
-                onChange={handleEndingDateChange}
-              /> */}
-              <Button type='submit'>Create</Button>
-            </form>
-          </div>
-        </Container>
-      </MuiPickersUtilsProvider>
-    </>
+    <Container maxWidth='md'>
+      <div>
+        <Typography component='h1' variant='h5'>
+          Create New Schedule
+        </Typography>
+        <NewScheduleForm />
+      </div>
+    </Container>
   );
 };
 
