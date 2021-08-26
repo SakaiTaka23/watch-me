f451e5d1b7dd0e98007cc34533ed327bb469079d
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-26 17:51:49 +0900

    現時点でのスケジュール作成画面保存

--- frontend/pages/schedule/new.tsx
+++ frontend/pages/schedule/new.tsx
@@ -1,44 +1,107 @@
+import { Container, Typography, TextField, Button, MenuItem, Select } from '@material-ui/core';
+import { Picker } from 'emoji-mart';
 import React from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import useCreateSchedule from '../../hooks/api/schedule/useCreateSchedule';
 import { Schedule } from '../../types/model/schedule';
+import 'emoji-mart/css/emoji-mart.css';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 const New = () => {
   const { createSchedule, id, error } = useCreateSchedule();
+  const { handleSubmit, register } = useForm();
+  const [emoji, setEmoji] = useState<string>('');
+  const [year, setYear] = useState(2021);
+  const [selectedStartingDate, handleStartingDateChange] = useState(new Date());
+  const [selectedEndingDate, handleEndingDateChange] = useState(new Date());
 
-  const testAllSchedule: Schedule = {
-    about: 'string',
-    emoji: 'string',
-    year: 2021,
-    month: 8,
-    day: 20,
-    start_time: '14:15',
-    end_time: '14:15',
-    place: 'string',
-    title: 'string',
-    url: 'http://example.com',
+  const handleClick = (data: any) => {
+    console.log('clicked');
+    console.log(data);
+    // createSchedule(testAllSchedule);
+    // console.log(error);
   };
 
-  //   const testSomeSchedule: Schedule = {
-  //     emoji: 'string',
-  //     year: 2021,
-  //     month: 8,
-  //     day: 20,
-  //     start_time: '14:15',
-  //     title: 'string',
-  //   };
-
-  //   createSchedule(testSomeSchedule);
-  //   console.log(error);
-
-  const handleClick = () => {
-    createSchedule(testAllSchedule);
-    console.log(error);
+  const handleYearChange = (e) => {
+    setYear(e.target.value as number);
+    console.log(year);
   };
 
   return (
     <>
-      <button onClick={handleClick}>submit</button>
-      <div>{id ? id : 'no id'}</div>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Container maxWidth='md'>
+          <div>
+            <Typography component='h1' variant='h5'>
+              Create New Schedule
+            </Typography>
+            <form onSubmit={handleSubmit(handleClick)}>
+              <TextField
+                variant='outlined'
+                margin='normal'
+                placeholder='Title *'
+                fullWidth
+                {...register('title', {
+                  required: true,
+                })}
+              />
+              <div suppressHydrationWarning={true}>
+                {process.browser && (
+                  <Picker
+                    onSelect={(e) => {
+                      setEmoji(e.id);
+                    }}
+                  />
+                )}
+              </div>
+              <TextField
+                value={emoji}
+                variant='outlined'
+                margin='normal'
+                placeholder='Please Pick An Emoji'
+                fullWidth
+                {...register('emoji', {
+                  required: true,
+                })}
+              />
+              <TextField
+                variant='outlined'
+                margin='normal'
+                placeholder='About'
+                minRows='5'
+                maxRows='10'
+                multiline
+                fullWidth
+                {...register('about')}
+              />
+              <TextField variant='outlined' margin='normal' placeholder='Place' fullWidth {...register('place')} />
+              <TextField variant='outlined' margin='normal' placeholder='URL' fullWidth {...register('url')} />
+              {/* 2021~2025の間の範囲に収まるべき */}
+              <input type='number' value={year} readOnly {...register('year')} />
+              {/* <DatePicker
+                disableToolbar
+                variant='inline'
+                label='Only calendar'
+                helperText='No year selection'
+                value={selectedStartingDate}
+                onChange={handleStartingDateChange}
+              />
+
+              <DatePicker
+                disableToolbar
+                variant='inline'
+                label='Only calendar'
+                helperText='No year selection'
+                value={selectedEndingDate}
+                onChange={handleEndingDateChange}
+              /> */}
+              <Button type='submit'>Create</Button>
+            </form>
+          </div>
+        </Container>
+      </MuiPickersUtilsProvider>
     </>
   );
 };
