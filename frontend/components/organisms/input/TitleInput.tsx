39b4e3bd021a38f47108a41d1bb6119efd5405a6
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-26 21:05:09 +0900

    タイトルがなかった場合のエラーメッセージを表示するようにした

--- frontend/components/organisms/input/TitleInput.tsx
+++ frontend/components/organisms/input/TitleInput.tsx
@@ -1,20 +1,30 @@
-import { TextField } from '@material-ui/core';
+import { TextField, Typography } from '@material-ui/core';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
 const TitleInput = () => {
-  const { register } = useFormContext();
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
 
   return (
-    <TextField
-      variant='outlined'
-      margin='normal'
-      placeholder='Title *'
-      fullWidth
-      {...register('title', {
-        required: true,
-      })}
-    />
+    <>
+      <TextField
+        variant='outlined'
+        margin='normal'
+        placeholder='Title *'
+        fullWidth
+        {...register('title', {
+          required: 'title is required',
+        })}
+      />
+      {errors.title && (
+        <Typography color='error' variant='overline'>
+          {errors.title.message}
+        </Typography>
+      )}
+    </>
   );
 };
 export default TitleInput;
