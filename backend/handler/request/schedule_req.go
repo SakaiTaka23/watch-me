dab901e5602e2710379d236b819bf14e0c52cceb
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-29 20:14:26 +0900

    時間をリクエスト時は文字列で受け付けることにした

--- backend/handler/request/schedule_req.go
+++ backend/handler/request/schedule_req.go
@@ -8,14 +8,14 @@ import (
 )
 
 type CreateSchedule struct {
-	ID        string    `json:"id"`
-	About     string    `json:"about" validate:"omitempty,min=1,max=250"`
-	Emoji     string    `json:"emoji" validate:"required,min=1,max=20"`
-	StartDate time.Time `json:"start_time" validate:"required,datetime"`
-	EndDate   time.Time `json:"end_time" validate:"omitempty,datetime"`
-	Place     string    `json:"place" validate:"omitempty,min=1,max=50"`
-	Title     string    `json:"title" validate:"required,min=1,max=20"`
-	URL       string    `json:"url" validate:"omitempty,url"`
+	ID        string `json:"id"`
+	About     string `json:"about" validate:"omitempty,min=1,max=250"`
+	Emoji     string `json:"emoji" validate:"required,min=1,max=20"`
+	StartDate string `json:"start_time" validate:"required"`
+	EndDate   string `json:"end_time" validate:"omitempty"`
+	Place     string `json:"place" validate:"omitempty,min=1,max=50"`
+	Title     string `json:"title" validate:"required,min=1,max=20"`
+	URL       string `json:"url" validate:"omitempty,url"`
 	UserID    string
 }
 
@@ -28,6 +28,16 @@ func (s *CreateSchedule) Validate() error {
 }
 
 func (s *CreateSchedule) ChangeStruct() *model.Schedule {
-	schedule := model.Schedule(*s)
+	var schedule model.Schedule
+	schedule.About = s.About
+	schedule.Emoji = s.Emoji
+	schedule.StartDate, _ = time.Parse("2006/01/02 15:00", s.StartDate)
+	schedule.EndDate, _ = time.Parse("2006/01/02 15:00", s.EndDate)
+	if schedule.EndDate.IsZero() {
+		schedule.EndDate = time.Time{}
+	}
+	schedule.Place = s.Place
+	schedule.Title = s.Title
+	schedule.URL = s.URL
 	return &schedule
 }
