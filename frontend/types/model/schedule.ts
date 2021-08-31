b5a13a6b35ee96a97503f2bcd4537c631bedee4d
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-31 18:23:24 +0900

    モデルの形を変更

--- frontend/types/model/schedule.ts
+++ frontend/types/model/schedule.ts
@@ -2,9 +2,6 @@ type Schedule = {
   id?: string;
   about?: string;
   emoji: string;
-  year: number;
-  month: number;
-  day: number;
   start_time: string;
   end_time?: string;
   place?: string;
