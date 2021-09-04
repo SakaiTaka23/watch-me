b92be507066f3ad9d27186392b9743ca72f34657
SakaiTaka23 <tt3.sakai@gmail.com>
2021-09-04 18:36:40 +0900

    フックを現在のルートに会うように変更

--- frontend/hooks/api/user/useGetSchedules.ts
+++ frontend/hooks/api/user/useGetSchedules.ts
@@ -2,8 +2,8 @@ import useSWR from 'swr';
 import { Schedule } from '../../../types/model/schedule';
 import { fetcher } from '../axios';
 
-const useGetSchedules = (username: string, year: number, month: number) => {
-  const { data, error } = useSWR<Schedule[], any>(`user/${username}?year=${year}&month=${month}`, fetcher);
+const useGetSchedules = (schedule_title: string, year: number, month: number) => {
+  const { data, error } = useSWR<Schedule[], any>(`user/${schedule_title}/${year}/${month}`, fetcher);
 
   return {
     schedules: data,
