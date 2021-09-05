27cb765c710a90c19598d5b7d1fbb383d8e973fc
SakaiTaka23 <tt3.sakai@gmail.com>
2021-09-05 14:52:33 +0900

    時間フォーマットをするための関数を定義

--- frontend/types/model/schedule.ts
+++ frontend/types/model/schedule.ts
@@ -10,4 +10,21 @@ type Schedule = {
   user_id: string;
 };
 
+const TimeFormat = (schedule: Schedule) => {
+  const start_time = new Date(schedule.start_time);
+  const start_format = `${start_time.getFullYear()}年${
+    start_time.getMonth() + 1
+  }月${start_time.getDate()}日${start_time.getHours()}:${
+    (start_time.getMinutes() < 10 ? '0' : '') + start_time.getMinutes()
+  }`;
+  const end_time = new Date(schedule?.end_time);
+  if (end_time == null) {
+    return start_format;
+  } else {
+    const end_format = `${end_time.getHours()}:${(end_time.getMinutes() < 10 ? '0' : '') + end_time.getMinutes()}`;
+    return `${start_format}〜${end_format}`;
+  }
+};
+
 export type { Schedule };
+export default TimeFormat;
