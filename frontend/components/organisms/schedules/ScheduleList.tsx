baf264062088511105ac9bbc9b80b5195ec1159c
SakaiTaka23 <tt3.sakai@gmail.com>
2021-09-07 18:15:38 +0900

    apiを叩くコンポーネントを変更

--- frontend/components/organisms/schedules/ScheduleList.tsx
+++ frontend/components/organisms/schedules/ScheduleList.tsx
@@ -5,10 +5,36 @@ import ScheduleCard from './ScheduleCard';
 
 type Props = {
   schedule_title: string;
-  schedules: Schedule[];
 };
 
-const ScheduleList: FC<Props> = ({ schedule_title, schedules }) => {
+const schedules: Schedule[] = [
+  {
+    id: '497f6eca-6276-4993-bfeb-53cbbbba6f07',
+    about: 'about',
+    emoji: 'leg',
+    start_time: '2021-07-28T15:00',
+    end_time: '',
+    place: 'place',
+    title: 'title',
+    url: 'http://example.com',
+    user_id: 'a169451c-8525-4352-b8ca-070dd449a1a5',
+  },
+  {
+    id: '497f6eca-6276-4993-bfeb-53cbbbba6f08',
+    about: 'about',
+    emoji: 'leg',
+    start_time: '2021-07-28T15:00',
+    end_time: '2021-07-28T15:00',
+    place: 'place',
+    title: 'title',
+    url: 'http://example.com',
+    user_id: 'a169451c-8525-4352-b8ca-070dd449a1a5',
+  },
+];
+
+const ScheduleList: FC<Props> = ({ schedule_title }) => {
+  // const { schedules, isLoading, isError } = useGetSchedules(schedule_title, year, month);
+
   return (
     <Grid container direction='column' spacing={2}>
       {schedules.map((schedule) => {
--- frontend/pages/user/[schedule_title].tsx
+++ frontend/pages/user/[schedule_title].tsx
@@ -4,44 +4,16 @@ import ScheduleList from '../../components/organisms/schedules/ScheduleList';
 import Selector from '../../components/organisms/selector/Selector';
 import useGetSchedules from '../../hooks/api/user/useGetSchedules';
 import { PeriodProvider } from '../../hooks/assets/usePeriod';
-import { Schedule } from '../../types/model/schedule';
 
 type Props = {
   schedule_title: string;
 };
 
-const schedules: Schedule[] = [
-  {
-    id: '497f6eca-6276-4993-bfeb-53cbbbba6f07',
-    about: 'about',
-    emoji: 'leg',
-    start_time: '2021-07-28T15:00',
-    end_time: '',
-    place: 'place',
-    title: 'title',
-    url: 'http://example.com',
-    user_id: 'a169451c-8525-4352-b8ca-070dd449a1a5',
-  },
-  {
-    id: '497f6eca-6276-4993-bfeb-53cbbbba6f08',
-    about: 'about',
-    emoji: 'leg',
-    start_time: '2021-07-28T15:00',
-    end_time: '2021-07-28T15:00',
-    place: 'place',
-    title: 'title',
-    url: 'http://example.com',
-    user_id: 'a169451c-8525-4352-b8ca-070dd449a1a5',
-  },
-];
-
 const GetUserSchedule: FC<Props> = ({ schedule_title }) => {
-  // const { schedules, isLoading, isError } = useGetSchedules(schedule_title, year, month);
-
   return (
     <PeriodProvider>
       <Selector />
-      <ScheduleList schedule_title={schedule_title} schedules={schedules} />
+      <ScheduleList schedule_title={schedule_title} />
     </PeriodProvider>
   );
 };
