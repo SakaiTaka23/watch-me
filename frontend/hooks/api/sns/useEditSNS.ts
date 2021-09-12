953a160a0d7cc944a9404ce87d13eea4eb2b932f
SakaiTaka23 <tt3.sakai@gmail.com>
2021-09-12 17:32:55 +0900

    sns取得更新用のフック作成

---
+++ frontend/hooks/api/sns/useEditSNS.ts
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import useSWR from 'swr';
+import { SNS } from '../../../types/model/sns';
+import { AuthContext } from '../../firebase/authContext';
+import { axiosInstance } from '../axios';
+
+const useEditSNS = () => {
+  const { token } = useContext(AuthContext);
+  const headers = { headers: { Authorization: `Bearer ${token}` } };
+  const fetcher = (url: string) => axiosInstance.get(url, headers).then((res) => res.data);
+  const { data, error } = useSWR<SNS[], any>(`sns`, fetcher);
+
+  return {
+    userSNS: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
+
+export default useEditSNS;
---
+++ frontend/hooks/api/sns/useUpdateSNS.ts
@@ -0,0 +1,21 @@
+import { AxiosError } from 'axios';
+import { useContext, useState } from 'react';
+import { SNS } from '../../../types/model/sns';
+import { AuthContext } from '../../firebase/authContext';
+import { axiosInstance } from '../axios';
+
+const useUpdateSNS = () => {
+  const { token } = useContext(AuthContext);
+  const [error, setError] = useState<AxiosError>(null);
+  const headers = { headers: { Authorization: `Bearer ${token}` } };
+
+  const updateSNS = (sns: SNS[]) => {
+    const data = { sns: sns };
+    axiosInstance.patch('/sns', data, headers).catch((e: AxiosError) => {
+      setError(e);
+    });
+  };
+  return { updateSNS, error };
+};
+
+export default useUpdateSNS;
