88a4dac9a7b8fd8c01e53ea3f8f3bdf20e2ec438
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-12 18:10:09 +0900

    snsの個別表示用コンポーネント作成(未完成)

---
+++ frontend/components/organisms/sns/SNSUpdateInput.tsx
@@ -0,0 +1,41 @@
+import { TextField, Typography } from '@material-ui/core';
+import React, { FC, useEffect } from 'react';
+import { useFieldArray, useFormContext } from 'react-hook-form';
+import { SNS } from '../../../types/model/sns';
+
+type Props = {
+  sns: SNS[];
+};
+
+const SNSUpdateInput: FC<Props> = ({ sns }) => {
+  const { register, control } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'sns',
+  });
+
+  useEffect(() => {
+    append({ sns: { sns } });
+    console.log(fields);
+    console.log('appended');
+  }, []);
+
+  return (
+    <>
+      <Typography>SNS</Typography>
+      <button type='button' onClick={() => append({ url: 'url appended' })}>
+        append
+      </button>
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <input placeholder='url' {...register(`sns.${index}.url` as const)} />
+          <button type='button' onClick={() => remove(index)}>
+            remove
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SNSUpdateInput;
