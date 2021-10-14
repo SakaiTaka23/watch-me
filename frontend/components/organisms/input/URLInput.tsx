a2012902df3929fb4e9ed0e3df254b8833214d03
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-14 12:25:58 +0900

    urlのバリデーション追加

--- frontend/components/organisms/input/URLInput.tsx
+++ frontend/components/organisms/input/URLInput.tsx
@@ -1,10 +1,30 @@
-import { TextField } from '@material-ui/core';
+import { TextField, Typography } from '@material-ui/core';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
 const URLInput = () => {
-  const { register } = useFormContext();
-  return <TextField variant='outlined' margin='normal' placeholder='URL' fullWidth {...register('url')} />;
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <>
+      <TextField
+        variant='outlined'
+        margin='normal'
+        placeholder='URL'
+        fullWidth
+        {...register('url', {
+          pattern: /https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+/,
+        })}
+      />
+      {errors.url && (
+        <Typography color='error' variant='overline'>
+          must be valid url
+        </Typography>
+      )}
+    </>
+  );
 };
 
 export default URLInput;
