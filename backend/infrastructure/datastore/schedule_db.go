e79a8235d79c059630726519bee213d8398a95bf
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-15 13:18:53 +0900

    DB操作実装

--- backend/infrastructure/datastore/schedule_db.go
+++ backend/infrastructure/datastore/schedule_db.go
@@ -27,10 +27,23 @@ func (scheduleRepo *ScheduleRepository) DeleteSchedule(id string) {
 	scheduleRepo.MySQLHandler.Conn.Where("id = ?", id).Delete(&model.Schedule{})
 }
 
+func (scheduleRepo *ScheduleRepository) FindFromUserID(user_id string, page int) *[]model.Schedule {
+	var schedules []model.Schedule
+	scheduleRepo.MySQLHandler.Conn.Where("user_id = ?", user_id).Scopes(paginate(page, 20)).Find(&schedules)
+	return &schedules
+}
+
 func (scheduleRepo *ScheduleRepository) GetScheduleInfo(id string) (*model.Schedule, error) {
 	var schedule model.Schedule
 	if err := scheduleRepo.MySQLHandler.Conn.Where("id = ?", id).First(&schedule).Error; errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, err
 	}
 	return &schedule, nil
+}
+
+func paginate(page int, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
 }
