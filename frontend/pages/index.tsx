148378e53fd348d207f86a854825c66860d047db
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-16 13:36:15 +0900

    認証確認出力をけした

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,11 +2,9 @@ import React, { useContext } from 'react';
 import ScheduleList from '../components/organisms/schedules/ScheduleList';
 import useGetUserSchedules from '../hooks/api/schedule/useGetUserSchedules';
 import { AuthContext } from '../hooks/firebase/authContext';
-import { useFirebase } from '../hooks/firebase/useFirebase';
 
 const Index = () => {
   const { user } = useContext(AuthContext);
-  const { Logout } = useFirebase();
   const { data, isLoading } = useGetUserSchedules();
 
   if (isLoading) {
@@ -15,16 +13,7 @@ const Index = () => {
 
   return (
     <div>
-      {user ? (
-        <>
-          <pre>{user.displayName + 'でログインしています'}</pre>
-          <button onClick={() => Logout()}>Logout</button>
-
-          <ScheduleList schedule_title={data.title} schedules={data.schedules} />
-        </>
-      ) : (
-        <h1>ログインしていません</h1>
-      )}
+      {user ? <ScheduleList schedule_title={data.title} schedules={data.schedules} /> : <h1>ログインしていません</h1>}
     </div>
   );
 };
