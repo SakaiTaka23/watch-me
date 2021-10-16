df5aaf9e2f39c7b366848871bd0a263db7c13d6c
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-16 12:54:52 +0900

    ヘッダー作成

---
+++ frontend/components/organisms/header/Header.tsx
@@ -0,0 +1,58 @@
+import { AppBar, Box, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../hooks/firebase/authContext';
+import Link from 'next/link';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const { user } = useContext(AuthContext);
+  return (
+    <Box className={classes.root} mb={3}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Link href='/' passHref>
+            <Typography variant='h6' className={classes.title}>
+              <a>Watch-ME</a>
+            </Typography>
+          </Link>
+          {user ? (
+            <>
+              <Box display='flex' mx={3}>
+                <Link href='/'>
+                  <Typography variant='subtitle1'>
+                    <a>MY PAGE</a>
+                  </Typography>
+                </Link>
+              </Box>
+              <Link href='logout' passHref>
+                <Typography variant='subtitle1'>
+                  <a>LOGOUT</a>
+                </Typography>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href='signin'>
+                <a>SIGN IN</a>
+              </Link>
+              <Link href='signup'>
+                <a>SIGN UP</a>
+              </Link>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
