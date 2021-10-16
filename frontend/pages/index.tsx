895d3763fe34d7691adc2ae82422034253c5e2de
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-16 16:01:24 +0900

    ローディングが必要な箇所にコンポーネントを入れた

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import Landing from '../components/organisms/landing/Landing';
+import Loading from '../components/organisms/loading/Loading';
 import ScheduleList from '../components/organisms/schedules/ScheduleList';
 import useGetUserSchedules from '../hooks/api/schedule/useGetUserSchedules';
 import { AuthContext } from '../hooks/firebase/authContext';
@@ -9,7 +10,7 @@ const Index = () => {
   const { data, isLoading } = useGetUserSchedules();
 
   if (isLoading) {
-    return <h1>loading</h1>;
+    return <Loading />;
   }
 
   return <div>{user ? <ScheduleList schedule_title={data.title} schedules={data.schedules} /> : <Landing />}</div>;
