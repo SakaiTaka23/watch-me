fb115d697ef15adb6b80c66110285cb3ac48a994
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-17 13:34:17 +0900

    更新するデータが取得できることを確認

---
+++ frontend/components/organisms/form/ScheduleUpdateForm.tsx
@@ -0,0 +1,62 @@
+import { Container, makeStyles } from '@material-ui/core';
+import React, { FC } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import useEditSchedule from '../../../hooks/api/schedule/useEditSchedule';
+import useUpdateSchedule from '../../../hooks/api/schedule/useUpdateSchedule';
+import SubmitButton from '../../molecules/SubmitButton';
+import Loading from '../loading/Loading';
+
+type Props = {
+  id: string;
+};
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const ScheduleUpdateForm: FC<Props> = ({ id }) => {
+  const classes = useStyles();
+  const { schedule, isLoading } = useEditSchedule(id);
+  const { updateSchedule } = useUpdateSchedule();
+  const methods = useForm({ defaultValues: schedule });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  console.log(schedule);
+
+  const handleClick = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <div className={classes.paper}>
+        <FormProvider {...methods}>
+          <form className={classes.form} onSubmit={methods.handleSubmit(handleClick)}>
+            <SubmitButton />
+          </form>
+        </FormProvider>
+      </div>
+    </Container>
+  );
+};
+
+export default ScheduleUpdateForm;
